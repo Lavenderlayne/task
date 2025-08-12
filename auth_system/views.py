740214c7f3bc6d312c257b1b89945d6870bbd46f
Lavenderlayne <evgentimcuk12@gmail.com>
2025-08-12 20:08:47 +0300

    Add TaskListView to display user-specific tasks and enforce login requirement

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from auth_system.forms import LoginForm, RegisterForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from task_app.models import Task
 
-class CustomLoginView(LoginView):
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "task_list.html"
+    context_object_name = "tasks"
+    def get_queryset(self):
+        queryset = Task.objects.filter(created_by=self.request.user)
+        return queryset
+
+class CustomLoginView(LoginRequiredMixin, LoginView):
     template_name = "login_page.html"
     form_class = LoginForm
 
-class RegisterView(CreateView):
+class RegisterView(LoginRequiredMixin,CreateView):
     template_name = "register_page.html"
     form_class = RegisterForm
     success_url = '/login/'
